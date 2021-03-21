c6249ca9c8fea5863e494efdb48c26d7f7746967
Anatol Kerbs <anker2702@gmail.com>
2021-03-21 21:36:16 +0300

    c6

--- js/app.js
+++ js/app.js
@@ -19,3 +19,16 @@ async function loadPosts() {
   const container = document.querySelector("#posts");
   container.innerHTML = data.map(toCard).join("\n");
 }
+
+function toCard(post) {
+  return `
+    <div class="card">
+      <div class="card-title">
+        ${post.title}
+      </div>
+      <div class="card-body">
+        ${post.body}
+      </div>
+    </div>
+  `;
+}
